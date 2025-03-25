63b3432097bc4275f12141ea03832bd8d448c621
imethmethsuka <saimethmethsuka@gmail.com>
2025-03-25 22:35:52 +0530

    Created Gifto loading screen and add obScrren1

---
+++ app/src/main/java/com/imethsuka/gifto/obScreen1.kt
@@ -0,0 +1,11 @@
+package com.imethsuka.gifto
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class obScreen1 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_ob_screen1)
+    }
+}
--- app/src/main/java/com/imethsuka/gifto/obScreen2.kt
+++ app/src/main/java/com/imethsuka/gifto/obScreen2.kt
@@ -1,31 +1,20 @@
 package com.imethsuka.gifto
 
-import android.content.Intent
 import android.os.Bundle
-import android.os.Handler
-import android.os.Looper
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+class obScreen2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-
+        setContentView(R.layout.activity_ob_screen2)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-
-        // Delay for 2 seconds (2000 milliseconds) then start obScreen1
-        Handler(Looper.getMainLooper()).postDelayed({
-            val intent = Intent(this, obScreen1::class.java)
-            startActivity(intent)
-            finish() // Optional: remove MainActivity from back stack
-        }, 2000)
     }
 }
